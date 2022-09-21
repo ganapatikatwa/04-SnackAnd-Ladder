0d764a685e01d90273f4219894bcbd546d094b16
[ganapat] <ganapatisakatwa@gmail.com>
2022-09-21 10:21:17 +0530

    [Ganapati] Player Position Zero

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Problem");
+            SnakeLadder obj= new SnakeLadder();
+            obj.SnakeNLadder();
+        }
+    }
+}
